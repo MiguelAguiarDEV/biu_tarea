549adea58d62fa586a8c14618f9fa9f800be2f40
Miguel Santiesteban Aguiar <miguel.santiesteban.aguiar@gmail.com>
2025-01-09 19:54:06 +0000

    aaaaa

--- exec.sh
+++ exec.sh
@@ -16,7 +16,6 @@ export DB_HOST
 export DB_PORT
 export DB_NAME
 
-
 # Funciones para cada operación
 setupEnv() {
     echo "Configurando el entorno virtual..."
@@ -36,9 +35,11 @@ setupEnv() {
     if [ -f "requirements.txt" ]; then
         echo "Instalando dependencias desde requirements.txt..."
         pip install -r requirements.txt
-        wget http://archive.apache.org/dist/avro/avro-1.10.2/java/avro-tools-1.10.2.jar -O avro-tools.jar
+        wget http://archive.apache.org/dist/avro/avro-1.10.2/java/avro-tools-1.10.2.jar \
+            -O avro-tools.jar
         wget https://repo1.maven.org/maven2/org/apache/parquet/parquet-tools/1.12.3/parquet-tools-1.12.3.jar
-        java -cp parquet-tools-1.11.1.jar:/opt/hadoop/share/hadoop/common/hadoop-common-3.3.6.jar:/opt/hadoop/share/hadoop/common/lib/*:/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-3.3.6.jar org.apache.parquet.tools.Main cat 59feddf9-8253-448a-b08c-a56b74eb68c0.parquet
+        java -cp parquet-tools-1.11.1.jar:/opt/hadoop/share/hadoop/common/hadoop-common-3.3.6.jar:/opt/hadoop/share/hadoop/common/lib/*:/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-3.3.6.jar \
+            org.apache.parquet.tools.Main cat 59feddf9-8253-448a-b08c-a56b74eb68c0.parquet
     else
         echo "Archivo requirements.txt no encontrado. No se instalaron dependencias."
     fi
@@ -46,52 +47,36 @@ setupEnv() {
     echo "Entorno virtual configurado."
 }
 
-testDB() {
-    echo "Probando la conexión a la base de datos..."
-
-    # Conectar a la base de datos
-    mariadb -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e "USE $DB_NAME;"
-
-    # Comprobar el código de salida
-    if [ $? -eq 0 ]; then
-        echo "Conexión exitosa a la base de datos."
-    else
-        echo "Error al conectar a la base de datos."
-    fi
-}
-
-# Done
 createDB() {
     echo "Creando la base de datos..."
     mariadb -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS < scripts/moviebind.sql
     echo "Base de datos creada."
 }
 
-# Done
 loadData() {
     echo "Cargando datos iniciales en la base de datos..."
     python3 scripts/inserciones.py
     echo "Datos cargados en la base de datos."
 }
 
-#Done
 deleteDB() {
     echo "Borrando todas las tablas de la base de datos..."
-    mariadb -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e "DROP DATABASE $DB_NAME;"
+    mariadb -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS \
+        -e "DROP DATABASE $DB_NAME;"
     echo "Base de datos borrada."
 }
 
 import() {
     read -p "Introduce el nombre de la tabla que deseas importar: " table
     echo "Importando datos de la tabla '$table' a HDFS..."
-    sqoop import --connect jdbc:mariadb://$DB_HOST:$DB_PORT/$DB_NAME 
-        --username $DB_USER 
-        --password $DB_PASS 
-        --table $table 
-        --target-dir $HDFS_PATH/imported_data/$table
-        --as-textfile 
+    sqoop import --connect jdbc:mariadb://$DB_HOST:$DB_PORT/$DB_NAME \
+        --username $DB_USER \
+        --password $DB_PASS \
+        --table $table \
+        --target-dir $HDFS_PATH/imported_data/$table \
+        --as-textfile \
         --driver org.mariadb.jdbc.Driver
-    echo "Datos importados a HDFS desde la tabla '$table'en $HDFS_PATH/imported_data/$table."
+    echo "Datos importados a HDFS desde la tabla '$table' en $HDFS_PATH/imported_data/$table."
 }
 
 importFilters() {
@@ -99,14 +84,14 @@ importFilters() {
     read -p "Introduce las columnas (por ejemplo: id_usuario,nickname): " columns
     read -p "Introduce la condición para filtrar los datos (por ejemplo, 'id_usuario > 5'): " condition
     echo "Importando datos de la tabla '$table' a HDFS con la condición '$condition'..."
-    sqoop import --connect jdbc:mariadb://$DB_HOST:$DB_PORT/$DB_NAME 
-        --username $DB_USER 
-        --password $DB_PASS 
-        --table $table 
-        --columns "$columns"
-        --where "$condition" 
-        --target-dir $HDFS_PATH/filtered_data/$table 
-        --as-textfile 
+    sqoop import --connect jdbc:mariadb://$DB_HOST:$DB_PORT/$DB_NAME \
+        --username $DB_USER \
+        --password $DB_PASS \
+        --table $table \
+        --columns "$columns" \
+        --where "$condition" \
+        --target-dir $HDFS_PATH/filtered_data/$table \
+        --as-textfile \
         --driver org.mariadb.jdbc.Driver
     echo "Datos importados con filtros de la tabla '$table' en $HDFS_PATH/filtered_data/$table."
 }
@@ -114,12 +99,12 @@ importFilters() {
 importAvro() {
     read -p "Introduce el nombre de la tabla que deseas importar en formato Avro: " table
     echo "Importando datos de la tabla '$table' a HDFS en formato Avro..."
-    sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME 
-        --username $DB_USER 
-        --password $DB_PASS 
-        --table $table 
-        --as-avrodatafile 
-        --target-dir $HDFS_PATH/avro_data/$table
+    sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME \
+        --username $DB_USER \
+        --password $DB_PASS \
+        --table $table \
+        --as-avrodatafile \
+        --target-dir $HDFS_PATH/avro_data/$table \
         --driver org.mariadb.jdbc.Driver
     echo "Datos de la tabla '$table' importados en formato Avro en $HDFS_PATH/avro_data/$table."
 }
@@ -127,11 +112,11 @@ importAvro() {
 importParquet() {
     read -p "Introduce el nombre de la tabla que deseas importar en formato Parquet: " table
     echo "Importando datos de la tabla '$table' a HDFS en formato Parquet..."
-    sqoop import sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME 
-        --username $DB_USER 
-        --password $DB_PASS 
-        --table $table 
-        --as-parquetfile 
+    sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME \
+        --username $DB_USER \
+        --password $DB_PASS \
+        --table $table \
+        --as-parquetfile \
         --target-dir $HDFS_PATH/parquet_data/$table
     echo "Datos de la tabla '$table' importados en formato Parquet en $HDFS_PATH/parquet_data/$table."
 }
@@ -139,28 +124,38 @@ importParquet() {
 importSnappy() {
     read -p "Introduce el nombre de la tabla que deseas importar en formato comprimido con Snappy: " table
     echo "Importando datos de la tabla '$table' a HDFS en formato comprimido con Snappy..."
-    sqoop import sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME 
-    --username $DB_USER 
-    --password $DB_PASS 
-    --table $table 
-    --as-textfile 
-    --compression-codec org.apache.hadoop.io.compress.SnappyCodec
-    --driver org.mariadb.jdbc.Driver
-    --target-dir $HDFS_PATH/snapy_data/$table
+    sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME \
+        --username $DB_USER \
+        --password $DB_PASS \
+        --table $table \
+        --as-textfile \
+        --compression-codec org.apache.hadoop.io.compress.SnappyCodec \
+        --driver org.mariadb.jdbc.Driver \
+        --target-dir $HDFS_PATH/snapy_data/$table
     echo "Datos comprimidos con Snappy de la tabla '$table' importados en $HDFS_PATH/snapy_data/$table."
 }
 
 importHive() {
     read -p "Introduce el nombre de la tabla que deseas importar en Hive: " table
     echo "Importando datos de la tabla '$table' a HDFS para Apache Hive..."
-    sqoop import sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME --username $DB_USER --password $DB_PASS --table $table --hive-import --create-hive-table --hive-table $table
+    sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME \
+        --username $DB_USER \
+        --password $DB_PASS \
+        --table $table \
+        --hive-import \
+        --create-hive-table \
+        --hive-table $table
     echo "Datos importados a Hive desde la tabla '$table'."
 }
 
 exportData() {
     read -p "Introduce el nombre de la tabla que deseas exportar desde HDFS a MariaDB: " table
     echo "Exportando datos desde HDFS a la tabla '$table' en MariaDB..."
-    sqoop export sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME --username $DB_USER --password $DB_PASS --table $table --export-dir $HDFS_PATH/$table
+    sqoop export --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME \
+        --username $DB_USER \
+        --password $DB_PASS \
+        --table $table \
+        --export-dir $HDFS_PATH/$table
     echo "Datos exportados a la tabla '$table' en MariaDB."
 }
 
@@ -168,7 +163,9 @@ exportTables() {
     for table in $(cat tablas.txt); do
         echo "Exportando tabla: $table"
         sqoop export \
-            sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME --username $DB_USER --password $DB_PASS --username root \
+            --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME \
+            --username $DB_USER \
+            --password $DB_PASS \
             --table $table \
             --export-dir /user/hadoop/text_data/$table \
             --input-fields-terminated-by ',' \
@@ -181,7 +178,9 @@ importTables() {
     for table in $(cat tablas.txt); do
         echo "Importando tabla: $table"
         sqoop import \
-            sqoop import --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME --username $DB_USER --password $DB_PASS --username root \
+            --connect jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME \
+            --username $DB_USER \
+            --password $DB_PASS \
             --table $table \
             --target-dir /user/hadoop/text_data/$table \
             --as-textfile \
@@ -197,17 +196,14 @@ case $1 in
     -createDB)
         createDB
         ;;
-    -importData)
-	    importData
-	;;
+    -import)
+        import
+        ;;
     -loadData)
         loadData
         ;;
     -deleteDB)
         deleteDB
-        ;;
-    -import)
-        importData
         ;;
     -importFilters)
         importFilters
@@ -232,4 +228,3 @@ case $1 in
         exit 1
         ;;
 esac
-
